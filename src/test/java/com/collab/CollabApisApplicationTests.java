aa88bf18fa7e34f2e789454773f2c2b88e9164fb
Tonmoy Chowdhury <tonmoy.chowdhury@maersk.com>
2023-02-28 18:23:28 +0000

    initial commit

---
+++ src/test/java/com/collab/CollabApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
